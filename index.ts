6673fcb4cdc3e144fa68cd5e551fe352af6af7ce
Xueyanlei <91644063+pldrwmhdkanr@users.noreply.github.com>
2025-09-29 16:05:55 +0800

    Replace Bun server implementation with a Node.js HTTP server

--- index.ts
+++
@@ -1,8 +0,0 @@
-const server = Bun.serve({
-  port: process.env.PORT || 3000,
-  fetch(req) {
-    return new Response("你好桑老师!");
-  },
-});
-
-console.log(`Listening on http://localhost:${server.port} ...`);
